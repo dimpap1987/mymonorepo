dafeaf88bd88186b88c8e52aa04e4334d9fd82de
dpapoutsis <dimitris.papoutsis@sword-group.com>
2022-10-21 23:18:38 +0300

    facebook login ui added

--- libs/shared/ui/google-button/src/lib/google-button.module.ts
+++ libs/shared/ui/google-button/src/lib/google-button.module.ts
@@ -1,14 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {GoogleButtonComponent} from './google-button/google-button.component';
+import {FacebookButtonComponent} from './facebook-button/facebook-button.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [GoogleButtonComponent],
-  exports: [
-    GoogleButtonComponent
-  ],
-  providers: []
+  declarations: [GoogleButtonComponent, FacebookButtonComponent],
+  exports: [GoogleButtonComponent, FacebookButtonComponent],
+  providers: [],
 })
 export class GoogleButtonModule {
 }
