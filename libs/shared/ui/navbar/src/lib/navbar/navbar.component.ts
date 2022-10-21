627f7c1dca4769193bbda964137d05bb6c909f95
dpapoutsis <dimitris.papoutsis@sword-group.com>
2022-10-21 20:47:44 +0300

    lib for variables,mixins - navbar component

--- libs/shared/ui/navbar/src/lib/navbar/navbar.component.ts
+++ libs/shared/ui/navbar/src/lib/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {Observable} from "rxjs";
-import {AuthService, isLogin, User} from "@mymonorepo/shared/utils";
+import {AuthService, getUser, User, UserState} from "@mymonorepo/shared/utils";
 import {Store} from "@ngrx/store";
 
 @Component({
@@ -10,14 +10,10 @@ import {Store} from "@ngrx/store";
 })
 export class NavbarComponent {
 
-  loggedIn = false;
-
-  isLogin: Observable<boolean> = this.store.select(isLogin);
+  user$: Observable<UserState> = this.store.select(getUser);
 
   constructor(private store: Store<{ user: User }>,
               private authService: AuthService) {
-
-    this.isLogin.subscribe(logged => this.loggedIn = logged)
   }
 
   logOut() {
