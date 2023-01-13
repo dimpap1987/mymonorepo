34fedb70d47c719a5c27132bba0b79e37f8c2127
dpapoutsis <dimitris.papoutsis@sword-group.com>
2023-01-13 22:18:28 +0200

    favicon added

--- libs/shared/ui/home/src/lib/landing-page/landing-page.component.ts
+++ libs/shared/ui/home/src/lib/landing-page/landing-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'landing-page',
+  selector: 'dp-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css'],
   encapsulation: ViewEncapsulation.Emulated,
