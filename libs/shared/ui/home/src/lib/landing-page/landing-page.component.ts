6f9ee9821b714e9083d024a4536cdf15f395fc2c
dpapoutsis <dimitris.papoutsis@sword-group.com>
2023-02-14 20:13:04 +0200

    landing page css

--- libs/shared/ui/home/src/lib/landing-page/landing-page.component.ts
+++ libs/shared/ui/home/src/lib/landing-page/landing-page.component.ts
@@ -1,11 +1,9 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'dp-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css'],
-  encapsulation: ViewEncapsulation.Emulated,
-})
+  styleUrls: ['./landing-page.component.css']})
 export class LandingPageComponent implements OnInit {
   constructor() {}
 
