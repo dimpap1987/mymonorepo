fdd23557d4ce204d9e9ccc3a37d3b14e18629c8d
dpapoutsis <dimitris.papoutsis@sword-group.com>
2023-03-06 00:41:37 +0200

    navbar, sidebar css

--- libs/shared/ui/toolbar/src/lib/shared-ui-toolbar.module.ts
+++ libs/shared/ui/toolbar/src/lib/shared-ui-toolbar.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { PanelMenuModule } from 'primeng/panelmenu'
 import { TieredMenuModule } from 'primeng/tieredmenu'
 import { MenuComponent } from './menu/menu.component'
 @NgModule({
-  imports: [CommonModule, TieredMenuModule],
+  imports: [CommonModule, TieredMenuModule, PanelMenuModule],
   declarations: [MenuComponent],
   exports: [MenuComponent],
 })
