1ce71874c40a35ecc7df6dbae0f46a2f77bcf025
dpapoutsis <dimitris.papoutsis@sword-group.com>
2023-04-11 03:18:48 +0300

    css changes

--- apps/api/src/app/strategies/github-strategy.ts
+++ apps/api/src/app/strategies/github-strategy.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { PassportStrategy } from '@nestjs/passport'
-import { Profile, Strategy } from 'passport-github'
+import { Strategy } from 'passport-github'
 
 @Injectable()
 export class GithubOauthStrategy extends PassportStrategy(Strategy, 'github') {
@@ -9,19 +9,21 @@ export class GithubOauthStrategy extends PassportStrategy(Strategy, 'github') {
       clientID: process.env.GITHUB_OAUTH_CLIENT_ID,
       clientSecret: process.env.GITHUB_OAUTH_CLIENT_SECRET,
       callbackURL: process.env.GITHUB_OAUTH_CALLBACK_URL,
-      scope: ['public_profile', 'read:project'],
+      scope: ['read:project', 'user:email', 'read:user'],
     })
   }
 
-  async validate(accessToken: string, _refreshToken: string, profile: Profile) {
+  async validate(accessToken: string, _refreshToken: string, profile: any, done: any) {
     //TODO maybe with should store the access token and refresh token in the db
+    console.log(profile)
+
     const { username, profileUrl, photos, id } = profile
-    return {
+    done(null, {
       profileUrl: profileUrl,
       githubUsername: username,
       picture: photos[0].value,
       profileId: id,
       accessTokenGithub: accessToken,
-    }
+    })
   }
 }
