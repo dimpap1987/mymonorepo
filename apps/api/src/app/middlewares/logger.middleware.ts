7f1378983c8e595ac725cd899d245c4ef7bf6e5d
dpapoutsis <dimitris.papoutsis@sword-group.com>
2023-04-14 02:29:56 +0300

    LoggerMiddleware email added

--- apps/api/src/app/middlewares/logger.middleware.ts
+++ apps/api/src/app/middlewares/logger.middleware.ts
@@ -1,14 +1,21 @@
+import { JwtTokenService } from '@mymonorepo/jwt-utils'
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common'
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
+  constructor(private jwtService: JwtTokenService) {}
+
   use(req, res, next) {
     const { ip, method, path: url } = req
+    const email = this.jwtService.extractPayload(req.cookies['accessToken'])?.user?.email
     const userAgent = req.get('user-agent') || ''
+
     res.on('close', () => {
       const { statusCode } = res
       const contentLength = res.get('content-length')
-      Logger.log(`${method} ${url} ${statusCode} ${contentLength} - ${userAgent} ${ip}`)
+      const emailLog = `${email ? `- email: ${email}` : ''}`
+
+      Logger.log(`${method} ${url} ${statusCode} ${contentLength} - ${userAgent} ${ip} ${emailLog}`)
     })
     next()
   }
