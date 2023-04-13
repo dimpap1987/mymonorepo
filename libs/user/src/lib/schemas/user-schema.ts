32db43040595347760c4263936004a419c5ada94
dpapoutsis <dimitris.papoutsis@sword-group.com>
2023-04-14 02:52:01 +0300

    User model, added constraints

--- libs/user/src/lib/schemas/user-schema.ts
+++ libs/user/src/lib/schemas/user-schema.ts
@@ -8,10 +8,10 @@ export class User {
   @Prop({ required: true })
   id?: string
 
-  @Prop()
+  @Prop({ index: true, unique: true })
   username?: string
 
-  @Prop({ required: true })
+  @Prop({ required: true, index: true, unique: true })
   email?: string
 
   @Prop()
