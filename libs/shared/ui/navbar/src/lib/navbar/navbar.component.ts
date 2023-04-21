819fa035751f1d751fc2f3a6712b260836ae4563
dpapoutsis <dimitris.papoutsis@sword-group.com>
2023-04-22 01:39:45 +0300

    navbar responsive - user options added

--- libs/shared/ui/navbar/src/lib/navbar/navbar.component.ts
+++ libs/shared/ui/navbar/src/lib/navbar/navbar.component.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  Renderer2,
+  ViewChild,
+} from '@angular/core'
 import { LoginDialogService } from '@mymonorepo/shared/ui/login'
 import { AuthService, getUser, User, UserState } from '@mymonorepo/shared/utils'
 import { Store } from '@ngrx/store'
@@ -11,18 +18,41 @@ import { Observable } from 'rxjs'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent {
+  @ViewChild('toggleButton') toggleButton: ElementRef
+  @ViewChild('profileOptions') profileOptions: ElementRef
+
   user$: Observable<UserState> = this.store.select(getUser)
+
+  profileOptionsShow = false
+  sidebarVisible = false
 
   constructor(
     private store: Store<{ user: User }>,
     private authService: AuthService,
-    private loginDialogService: LoginDialogService
-  ) {}
+    private loginDialogService: LoginDialogService,
+    private renderer: Renderer2,
+    private cdr: ChangeDetectorRef
+  ) {
+    //TODO make a directive
+    this.renderer.listen('window', 'click', (e: Event) => {
+      if (
+        !this.toggleButton?.nativeElement?.contains(e.target) &&
+        e.target !== this.profileOptions?.nativeElement
+      ) {
+        this.profileOptionsShow = false
+        this.cdr.detectChanges()
+      }
+    })
+  }
 
   logOut() {
     this.authService.logOut()
   }
   login() {
     this.loginDialogService.open()
+  }
+
+  toggleProfileOptions() {
+    this.profileOptionsShow = !this.profileOptionsShow
   }
 }
--- libs/shared/ui/navbar/src/lib/shared-ui-navbar.module.ts
+++ libs/shared/ui/navbar/src/lib/shared-ui-navbar.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { SharedUiLoginModule } from '@mymonorepo/shared/ui/login'
 import { ButtonModule } from 'primeng/button'
+import { SidebarModule } from 'primeng/sidebar'
 import { ToolbarModule } from 'primeng/toolbar'
 import { NavbarComponent } from './navbar/navbar.component'
-
 @NgModule({
-  imports: [CommonModule, ToolbarModule, ButtonModule, SharedUiLoginModule, RouterModule],
+  imports: [CommonModule, ToolbarModule, ButtonModule, SharedUiLoginModule, RouterModule, SidebarModule],
   declarations: [NavbarComponent],
   exports: [NavbarComponent],
 })
